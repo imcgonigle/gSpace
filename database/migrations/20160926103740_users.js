afe336d4ed0e0b39190ca850b0ca39ad2ce0f199
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-26 13:22:07 -0600

    Update user migrate

--- database/migrations/20160926103740_users.js
+++ database/migrations/20160926103740_users.js
@@ -3,10 +3,11 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(table) {
 		table.increments('id');
 		table.string('username');
+		table.string('avatar_url');
 		table.text('biography');
 		table.string('first_name');
 		table.string('last_name');
-		table.string('cohort');
+		table.integer('cohort');
 		table.string('city');
 		table.string('linkdn_url');
 		table.string('github_url');
@@ -14,10 +15,9 @@ exports.up = function(knex, Promise) {
 		table.string('email');
 		table.dateTime('first_login');
 		table.dateTime('last_login');
-
-	})
+	});
 };
 
 exports.down = function(knex, Promise) {
-	knex.schema.dropTable('users');
+	return knex.schema.dropTable('users');
 };
