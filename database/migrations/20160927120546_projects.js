76604a470a9e9654cd3130232148a33d3c78287b
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-27 12:30:16 -0600

    finish projects migration file

--- database/migrations/20160927120546_projects.js
+++ database/migrations/20160927120546_projects.js
@@ -3,9 +3,15 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function(table) {
 		table.increments('id');
 		table.integer('creator_id');
-	})
+		table.string('title');
+		table.body('text');
+		table.string('repository_url');
+		table.integer('likes');
+		table.dateTime('created_on');
+		table.dateTime('updated_on');
+	});
 };
 
 exports.down = function(knex, Promise) {
-
+	return knex.schema.dropTable('projects');
 };
