002e31fa4bc074b7064264759c29a5c94ff8c5fb
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-27 11:35:46 -0600

    fixed login and redirection

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,24 @@ var router = express.Router();
 var passport = require('../passport')
 
 router.get('/', function(req, res, next) {
-  
-if (req.isAuthenticated()) {
-    res.render('static/home', { user: req.user });
-} else {
-  res.render('/login');
-}
+
+	if (req.isAuthenticated()) {
+	    res.render('static/home', { user: req.user });
+	} else {
+	  res.redirect('/login');
+	};
+
+});
+
+router.get('/login', function(req, res, next) {
+
+	if (req.isAuthenticated()) {
+		res.redirect('/');
+	} else{
+		res.render('static/login');
+	};
+
+});
+
 
 module.exports = router;
