2b9a49b742df789366a6e430bd1d51a87dac9f9c
Your Name <creighton@creightonsoukup.com>
2016-09-27 11:20:54 -0600

    fixed oauth

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,19 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../passport')
 
+
 router.get('/github', passport.authenticate("github"))
 
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('../');
+
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/error' }),
+  function(req, res, next) {
+    res.redirect('/');
   }
 );
 
-router.get('/logout', function(req,res,next) {
+router.get('/github/logout', function(req,res,next) {
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
   req.logout();
   res.redirect('/')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+console.log(req.user)
+if (req.isAuthenticated()) {
+    res.render('index', { user: req.user });
+} else {
+  res.render('index');
+}
 });
 
 module.exports = router;
