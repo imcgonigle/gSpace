d75f310bc580ba95f4309ef6f938be7fc27af20b
Matt Donovan <mattxd12@gmail.com>
2016-09-27 12:13:34 -0600

    working on question form/query

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,10 @@ var router = express.Router();
 var passport = require('../passport')
 
 router.get('/', function(req, res, next) {
-  
+
 if (req.isAuthenticated()) {
     res.render('static/home', { user: req.user });
 } else {
   res.render('/login');
 }
-
 module.exports = router;
