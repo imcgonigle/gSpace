3e80766565085c935b08e85ad6c6cf7db261b260
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-28 13:48:06 -0600

    add edit route for projects

--- routes/projects.js
+++ routes/projects.js
@@ -29,6 +29,71 @@ router.get('/:id/page', function(req, res, next) {
 
 });
 
+router.get('/:id/edit', function(req, res, next) {
+
+	var project_id = req.params.id;
+
+	query.getProjectByID(project_id)
+	.then(function(data) {
+
+		var project = data[0];
+
+		if(project.user_id == req.user.id){
+			res.render('pojects/edit', {
+				user: req.user,
+				project: project
+			})
+		} else{
+			res.redirect('/projects/' + project.id + '/page');
+		}
+
+	})
+	.catch(function(err) {
+		return next(err);
+	});
+
+});
+
+router.post('/:id/edit', function(req, res, next) {
+
+	if(!req.isAuthenticated()) {
+		res.redirect('/login');
+	} else {
+
+		var project_id = req.params.id;
+
+		query.getProjectByID(post_id)
+		.then(function(data) {
+
+			var project = data[0];
+
+			if(project.user_id == req.user.id) {
+
+				var title = req.params.title;
+				var body = req.params.body;
+				var repository_url = req.params.repository_url;
+
+				query.updateProject(project_id, title, body, repository_url)
+				.then(function(id) {
+					res.redirect('/projects/' + id + '/page');
+				})
+				.catch(function(err) {
+					return next(err);
+				});
+
+			} else {
+				res.redirect('/projects/' + project_id + '/page')
+			}
+
+		})
+		.catch(function(err){
+			return next(err);
+		});
+
+	}
+
+});
+
 router.get('/new', function(req, res, next) {
 
 	if(req.isAuthenticated()) {
