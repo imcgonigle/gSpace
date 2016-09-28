77763aefdf06a18f13fea5cfde909027d3050a7f
Lisa Hull <lisa@jchull.com>
2016-09-28 09:22:25 -0600

    answers_comments migration + seed

---
+++ database/migrations/20160927221114_answers_comments.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('answers_comments', function(table) {
+		table.increments('id');
+    table.integer('users_id').references('id').inTable('users');
+    table.integer('answers_id').references('id').inTable('answers');
+		table.text('body');
+		table.integer('likes');
+		table.dateTime('created_on');
+		table.dateTime('updated_on');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('answers_comments');
+};
