a5afe36e7d7bcea61cbde47954eb0e88d669970d
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-28 16:28:23 -0600

    added creator_id to projects table

--- database/migrations/20160927120546_projects.js
+++ database/migrations/20160927120546_projects.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function(table) {
 		table.increments('id');
+		table.integer('creator_id');
     table.integer('users_id')
 		.references('id')
 		.inTable('users')
