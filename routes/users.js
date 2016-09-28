785ddee7fbf29676abf0e1a9517ed1a37b9567bc
Your Name <creighton@creightonsoukup.com>
2016-09-28 09:33:25 -0600

    worked on user page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/new', function(req, res, next) {
-  res.render('newUser', {user:req.user});
+  var user = req.user._json
+  res.render('newUser', {user:user});
 });
+
+router.post('/', function(req, res, next) {
+  knex('users').update({
+    name: req.body.name,
+    email: req.body.email,
+    cohort: req.body.cohort,
+    location: req.body.location,
+    biography: req.body.bio,
+    website: req.body.website,
+    github_url: req.body.github,
+    linkedin_url: req.body.linkedin
+  }).where('username', req.user.username)
+})
 
 module.exports = router;
