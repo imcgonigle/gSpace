45c0867d1432c7c14059f2780b802b660afe6a1e
Your Name <creighton@creightonsoukup.com>
2016-09-28 11:41:11 -0600

    work in progress, need to pull upstream

--- database/queries/resources.js
+++ database/queries/resources.js
@@ -11,3 +11,36 @@ function Resources_Tags() = {
 function Comments() = {
   return knex('comments')
 }
+
+module.exports = {
+  getAllResources: Resources,
+  getResourceById: function(resource_id) {
+    return Projects().where('id', resource_id);
+  },
+  addResource: function (user_id, title, description, link) {
+    return Resources().insert({
+      user_id: user_id,
+      title: title,
+      description: description,
+      link: link,
+      created_on: new Date(),
+      updated_on: new Date()
+    })
+    .returning('id');
+  },
+  updateResource: function (resource_id, title, description, link) {
+    return Resources().where('id', resource_id).update({
+      title: title,
+      description: description,
+      link: link,
+      updated_on: new Date();
+    })
+    .returning('id');
+  },
+  deleteResource: function (resource_id) {
+    return Resources().where('id', resource_id).del();
+  },
+  addLikeToResource: function (resource_id, likes) {
+    
+  }
+}
