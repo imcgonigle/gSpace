f857d26c0565dfea31e050e7134c067876357291
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-28 15:11:56 -0600

    using github id for user id

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 			.then(function (data) {
 				var user = data[0]
 				if (user == undefined) {
-					queries.createUser(req.user.username, req.user._json.avatar_url, req.user._json.github_url, req.user._json.email, req.user._json.bio, req.user._json.location, req.user._json.name)
+					queries.createUser(req.user.id, req.user.username, req.user._json.avatar_url, req.user._json.github_url, req.user._json.email, req.user._json.bio, req.user._json.location, req.user._json.name)
 					.then(function(data) {
 						res.redirect('/users/new')
 					})
