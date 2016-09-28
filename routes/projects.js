9af48a1bef224ecd29e8a7d3531faca8d302d24f
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-28 12:21:03 -0600

    add abitily for user to add new post

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../database/queries/projects');
 
 router.get('/', function(req, res, next) {
 	res.render('projects/index');
 });
+
+router.get('/new', function(req, res, next) {
+
+	if(req.isAuthenticated()) {
+		res.render('projects/new', {user: req.user})
+	} else {
+		res.redirect('/login')
+	};
+
+});
+
+router.post('/new', function(req, res, next) {
+
+	if(req.isAuthenticated()){
+
+		var creator_id = req.user.id;
+		var title = req.body.title;
+		var body = req.body.body;
+		var repository_url = req.body.repository_url;
+
+		query.addProject(creator_id, title, body, repository_url)
+		.then(function(project_id) {
+			res.redirect('/projects/project_id');
+		})
+		.catch(function(err) {
+			return next(err);
+		});
+
+	} else {
+		res.redirect('/login');
+	}
+
+})
 
 module.exports = router;
