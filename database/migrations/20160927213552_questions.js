94c7890c3f8b147ac15e77f4d51fe3805f2e7016
Lisa Hull <lisa@jchull.com>
2016-09-27 21:43:15 -0600

    questions table migration + seed

---
+++ database/migrations/20160927213552_questions.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('questions', function(table) {
+		table.increments('id');
+		table.integer('user_id');
+		table.string('title');
+		table.text('body');
+		table.integer('likes');
+		table.dateTime('created_on');
+		table.dateTime('updated_on');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('questions');
+};
