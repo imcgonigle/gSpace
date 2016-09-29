490e56ef83ef1c4c10c71b1043c7c9bcec4189ee
Lisa Hull <lisa@jchull.com>
2016-09-29 15:56:11 -0600

    minor fixes to prevent nodemon crash

--- database/queries/resources.js
+++ database/queries/resources.js
@@ -18,112 +18,113 @@ function Tags() {
 
 module.exports = {
 
-  getAllResources: function () {
-    return Resources().orderBy('created_on', 'desc')
-  },
-  getResourceById: function(resource_id) {
-    return Resources().where('id', resource_id);
-  },
-  addResource: function (users_id, title, description, link) {
-    return Resources().insert({
-      users_id: users_id,
-      title: title,
-      description: description,
-      link: link,
-      created_on: new Date(),
-      updated_on: new Date()
-    })
-    .returning('id');
-  },
-  updateResource: function (resource_id, title, description, link) {
-    return Resources().where('id', resource_id).update({
-      title: title,
-      description: description,
-      link: link,
-      updated_on: new Date()
-    })
-    .returning('id');
-  },
-  deleteResource: function (resource_id) {
-    return Resources().where('id', resource_id).del();
-  },
-  addLikeToResource: function (resource_id, likes) {
-    return Resources().where('id', resource_id).update({
-      likes: likes +=1
-    }).returning('likes','id')
-  },
-  getResourceLikes: function (resource_id) {
-    return Resources().where('id', resource_id)
-  },
-  getResourceComments: function (resource_id) {
-    return Resources().where('id')
-  },
-  getResourceTags: function (resource_id) {
-    return Resources().select('resource.id as resource_id', 'resource.title as resource_title', "resource.link as link", 'resource.likes as likes', 'description')
-    .then(function (resourceData) {
-      return Tags().join('resources_tags', 'tags.id', 'resources_tags.tag_id')
-      .then(function (tagData) {
-        // console.log(tagData)
-        for (var i = 0; i < resourceData.length; i++) {
-          resourceData[i].tags = []
-          for (var j = 0; j < tagData.length; j++) {
-            if (tagData[j].resource_id === resourceData[i].resource_id) {
-              resourceData[i]["tags"].push(tagData[j].name)
-            }
-          }
-        }
-        console.log(resourceData)
-        return resourceData
-      })
-    })
-    // .leftJoin('resources_tags', 'resource.id', 'resources_tags.resource_id')
-    // .join('tags', 'resources_tags.tag_id', 'tags.id')
-    // .orderBy('title')
-    getAllResources: function(){
+    getAllResources: function () {
         return Resources().orderBy('created_on', 'desc')
     },
-    getResourceById: function(resource_id) {
+    getResourceById: function (resource_id) {
         return Resources().where('id', resource_id);
     },
-    addResource: function(users_id, title, description, link) {
+    addResource: function (users_id, title, description, link) {
         return Resources().insert({
-                users_id: users_id,
-                title: title,
-                description: description,
-                link: link,
-                created_on: new Date(),
-                updated_on: new Date()
-            })
+            users_id: users_id,
+            title: title,
+            description: description,
+            link: link,
+            created_on: new Date(),
+            updated_on: new Date()
+        })
             .returning('id');
     },
-    updateResource: function(resource_id, title, description, link) {
+    updateResource: function (resource_id, title, description, link) {
         return Resources().where('id', resource_id).update({
-                title: title,
-                description: description,
-                link: link,
-                updated_on: new Date()
-            })
+            title: title,
+            description: description,
+            link: link,
+            updated_on: new Date()
+        })
             .returning('id');
     },
-    deleteResource: function(resource_id) {
+    deleteResource: function (resource_id) {
         return Resources().where('id', resource_id).del();
     },
-    addLikeToResource: function(resource_id, likes) {
+    addLikeToResource: function (resource_id, likes) {
         return Resources().where('id', resource_id).update({
             likes: likes += 1
         }).returning('likes', 'id')
     },
-    getResourceLikes: function(resource_id) {
+    getResourceLikes: function (resource_id) {
         return Resources().where('id', resource_id)
     },
-    getResourceComments: function(resource_id) {
+    getResourceComments: function (resource_id) {
         return Resources().where('id')
     },
-    getResourceTags: function(resource_id) {
-        return Resources().join('users','resource.users_id','users.id').select('avatar_url','resource.id as resource_id', 'resource.title as resource_title', "resource.link as link", 'resource.likes as likes', 'description', 'users_id')
-            .then(function(resourceData) {
+    getResourceTags: function (resource_id) {
+        return Resources().select('resource.id as resource_id', 'resource.title as resource_title', "resource.link as link", 'resource.likes as likes', 'description')
+            .then(function (resourceData) {
                 return Tags().join('resources_tags', 'tags.id', 'resources_tags.tag_id')
-                    .then(function(tagData) {
+                    .then(function (tagData) {
+                        // console.log(tagData)
+                        for (var i = 0; i < resourceData.length; i++) {
+                            resourceData[i].tags = []
+                            for (var j = 0; j < tagData.length; j++) {
+                                if (tagData[j].resource_id === resourceData[i].resource_id) {
+                                    resourceData[i]["tags"].push(tagData[j].name)
+                                }
+                            }
+                        }
+                        console.log(resourceData)
+                        return resourceData
+                    })
+            })
+    },
+    // .leftJoin('resources_tags', 'resource.id', 'resources_tags.resource_id')
+    // .join('tags', 'resources_tags.tag_id', 'tags.id')
+    // .orderBy('title')
+    getAllResources: function () {
+        return Resources().orderBy('created_on', 'desc')
+    },
+    getResourceById: function (resource_id) {
+        return Resources().where('id', resource_id);
+    },
+    addResource: function (users_id, title, description, link) {
+        return Resources().insert({
+            users_id: users_id,
+            title: title,
+            description: description,
+            link: link,
+            created_on: new Date(),
+            updated_on: new Date()
+        })
+            .returning('id');
+    },
+    updateResource: function (resource_id, title, description, link) {
+        return Resources().where('id', resource_id).update({
+            title: title,
+            description: description,
+            link: link,
+            updated_on: new Date()
+        })
+            .returning('id');
+    },
+    deleteResource: function (resource_id) {
+        return Resources().where('id', resource_id).del();
+    },
+    addLikeToResource: function (resource_id, likes) {
+        return Resources().where('id', resource_id).update({
+            likes: likes += 1
+        }).returning('likes', 'id')
+    },
+    getResourceLikes: function (resource_id) {
+        return Resources().where('id', resource_id)
+    },
+    getResourceComments: function (resource_id) {
+        return Resources().where('id')
+    },
+    getResourceTags: function (resource_id) {
+        return Resources().join('users', 'resource.users_id', 'users.id').select('avatar_url', 'resource.id as resource_id', 'resource.title as resource_title', "resource.link as link", 'resource.likes as likes', 'description', 'users_id')
+            .then(function (resourceData) {
+                return Tags().join('resources_tags', 'tags.id', 'resources_tags.tag_id')
+                    .then(function (tagData) {
                         // console.log(tagData)
                         for (var i = 0; i < resourceData.length; i++) {
                             resourceData[i].tags = []
@@ -137,17 +138,16 @@ module.exports = {
                     })
             })
     },
-    addTagToResource: function(resource_id, tag_id) {
+    addTagToResource: function (resource_id, tag_id) {
         return Resources_Tags().insert({
             resource_id: resource_id,
             tag_id: tag_id
         })
     },
-    addTag: function(resource_id, tag_id) {
+    addTag: function (resource_id, tag_id) {
         return Tags().insert({
             name: name,
             created_at: new Date()
         })
     }
-
 }
